f1b6180d22b99618c2f4ff0037c69a36073ae2bd
emyagkikh <emyagkikh@mail.ru>
2020-05-22 00:58:11 +0700

    Complete module1/task14

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports.DEFAULT_COMMAND = `--generate`;
+
+module.exports.USER_ARGV_INDEX = 2;
+
+module.exports.ExitCode = {
+  success: 0,
+  error: 1,
+};
---
+++ src/service/cli/help.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  name: `--help`,
+  run() {
+    console.log(`Гайд: service.js <\command>\ Команды:--version: выводит номер версии -help печатает этот текст --generate <\count>\ формирует файл mocks.json`);
+  }
+};
+
+
